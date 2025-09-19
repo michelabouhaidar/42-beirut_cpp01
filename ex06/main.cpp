54ccca88d6a47cb5769ee47f0c72af10e85494d1
Michel Abou Haidar <mabou-ha@c2r6s4.42beirut.com>
2025-09-19 20:39:01 +0300

    cpp01

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mabou-ha <mabou-ha>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 22:56:01 by mabou-ha          #+#    #+#             */
-/*   Updated: 2025/09/09 23:21:27 by mabou-ha         ###   ########.fr       */
+/*   Updated: 2025/09/19 19:49:14 by mabou-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,18 @@ int main(int argc, char** argv)
 	Harl h;
 	int level;
 	if (argc != 2)
+	{
 		std::cout << "Usage: ./harlFilter <complain>" << std::endl;
+		return 1;
+	}
 	level = getLevel(argv[1]); 
 	switch(level)
 	{
+		case 3: h.complain("ERROR"); // fall through
+		case 2: h.complain("WARNING"); // fall through
+		case 1: h.complain("INFO"); // fall through
 		case 0: h.complain("DEBUG"); break;
-		case 1: h.complain("INFO"); break;
-		case 2: h.complain("WARNING"); break;
-		case 3: h.complain("ERROR"); break;
-		default: h.complain("invalid complain"); 
+		default: h.complain("Invalid complain"); 
 	}
 	return 0;
 }
